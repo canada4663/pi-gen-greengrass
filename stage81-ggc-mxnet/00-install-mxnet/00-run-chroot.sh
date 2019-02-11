3440409a703ba03f512d3e6063006522f190f460
canada4663 <3118399+canada4663@users.noreply.github.com>
2019-02-11 02:11:58 +0000

    UPDATE: update ggc-mxnet install to work with greengrass v1.7 and quickstart guidelines

--- stage81-ggc-mxnet/00-install-mxnet/00-run-chroot.sh
+++ stage81-ggc-mxnet/00-install-mxnet/00-run-chroot.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 cd /home/pi/ml/ggc-mxnet-v1.2.1-python-raspi/
+chmod 755 ./mxnet_installer.sh
 ./mxnet_installer.sh
+cd /home/pi/
+chown pi -R ml/ 
 
--- stage81-ggc-mxnet/00-install-mxnet/00-run.sh
+++ stage81-ggc-mxnet/00-install-mxnet/00-run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #extract mxnet tarball
-if [ -f ../files/ggc-mxnet-v1.2.1-python-raspi.tar.gz ]; then
+if [ -f files/ggc-mxnet-v1.2.1-python-raspi.tar.gz ]; then
   mkdir -p ${ROOTFS_DIR}/home/pi/ml/
-  tar Czxf ${ROOTFS_DIR}/home/pi/ml/ ../files/ggc-mxnet-v1.2.1-python-raspi.tar.gz 
-  chmod 755 ${ROOTFS_DIR}/home/pi/ml/ggc-mxnet-v1.2.1-python-raspi/mxnet_installer.sh
+  tar Czxf ${ROOTFS_DIR}/home/pi/ml/ files/ggc-mxnet-v1.2.1-python-raspi.tar.gz 
 else
   echo "files/ggc-* does not exist"
 fi
