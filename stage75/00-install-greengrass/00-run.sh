e5fe013e599a13bf25bcd6db0dfd819bcc768b6c
canada4663 <3118399+canada4663@users.noreply.github.com>
2019-02-11 02:07:20 +0000

    UPDATE: update greengrass to v1.7, including other minor tweaks to enable

--- stage75/00-install-greengrass/00-run.sh
+++ stage75/00-install-greengrass/00-run.sh
@@ -5,9 +5,12 @@ fs.protected_hardlinks = 1
 fs.protected_symlinks = 1
 EOF
 
-sed -i /$/cgroup_enable=memory cgroup_memory=1/ ${ROOTFS_DIR}/boot/cmdline.txt
+wget -O files/greengrass.tar.gz https://d1onfpft10uf5o.cloudfront.net/greengrass-core/downloads/1.7.0/greengrass-linux-armv7l-1.7.0.tar.gz
+wget -O files/root.ca.pem https://www.amazontrust.com/repository/AmazonRootCA1.pem
 
-tar Czxf ${ROOTFS_DIR} files/greengrass*.tar.gz
+sed -i 's/$/\ cgroup_enable=memory\ cgroup_memory=1/' ${ROOTFS_DIR}/boot/cmdline.txt
+
+tar Czxf ${ROOTFS_DIR} files/greengrass.tar.gz
 
 # Users should place the extracted keys/config into the fat32 partition.
 rm -rf ${ROOTFS_DIR}/greengrass/certs \
@@ -16,14 +19,16 @@ mkdir -p ${ROOTFS_DIR}/boot/greengrass/certs
 mkdir -p ${ROOTFS_DIR}/boot/greengrass/config
 
 # copy root cert into /greengrass/certs
-cp files/root.ca.pem ${ROOTFS_DIR}/greengrass/certs/
+cp files/root.ca.pem ${ROOTFS_DIR}/boot/greengrass/certs/
 
-# For twitch stream we want viewers to see our text BIG!
-# We also don't need to waste memory on GPU by default!
+# set resolution to 1280x720 for development
+# enable picamera by default
 cat <<EOF >>${ROOTFS_DIR}/boot/config.txt
 framebuffer_width=1280
 framebuffer_height=720
 gpu_mem=128
+start_x=1
+disable_camera_led=1
 EOF
 
 # Grab samples which also has the dependencies checker utility.
