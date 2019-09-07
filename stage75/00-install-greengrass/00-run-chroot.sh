4260cf1d274f5157fdfcfa608c2c960956585e90
canada4663 <3118399+canada4663@users.noreply.github.com>
2019-09-07 20:52:29 +0000

    WORKAROUND: temporary fix to solve for ggc fs overlay errors caused by kernel 4.19.57

--- stage75/00-install-greengrass/00-run-chroot.sh
+++ stage75/00-install-greengrass/00-run-chroot.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Temporary Fix to Allow Overlay File System to Work till GGC Fix Available for new kernels
+rpi-update eefe4b161f5e9730183c8dc1605e14c85b15cf51 
+
 adduser --disabled-password -q --gecos "" ggc_user
 addgroup ggc_group
 adduser ggc_user ggc_group
