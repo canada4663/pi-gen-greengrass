2850b5de6376e7ed4397eff135e8488ffb771923
canada4663 <3118399+canada4663@users.noreply.github.com>
2019-09-29 17:52:02 +0000

    FIX: change DLR installation to work with buster version of easy_install

--- stage76-ml-base/00-install-dlr/00-run-chroot.sh
+++ stage76-ml-base/00-install-dlr/00-run-chroot.sh
@@ -3,8 +3,10 @@
 #Install DLR
 cd /home/pi/ml/dlr/dlr*
 chmod 755 install-dlr.sh
+apt-get remove python-setuptools -y
 pip install setuptools
 pip install numpy
-easy_install dlr*.egg
+ls
+easy_install --no-deps dlr*.egg
 python tests/test-dlr.py
 
--- stage76-ml-base/00-install-dlr/00-run.sh
+++ stage76-ml-base/00-install-dlr/00-run.sh
@@ -6,6 +6,7 @@ mkdir -p ${ROOTFS_DIR}/home/pi/ml
 mkdir -p ${ROOTFS_DIR}/home/pi/ml/dlr
 tar Czxf ${ROOTFS_DIR}/home/pi/ml/dlr /tmp/dlr.tar.gz
 
+
 # Grab samples which also has the dependencies checker utility.
 if [ -d ${ROOTFS_DIR}/home/pi/aws-greengrass-samples ]; then
   cd ${ROOTFS_DIR}/home/pi/aws-greengrass-samples;
