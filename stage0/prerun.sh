fa26965a59ac30a7efd1419889021d62a2573d9f
canada4663 <3118399+canada4663@users.noreply.github.com>
2019-11-30 16:54:02 +0000

    TEST: diagnostic ls -lart at beginning of stage 0

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+ls -lart
+
+if [ -d ./work ]; then
+	ls -lart ./work/
+fi
+
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	bootstrap buster "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
 fi
